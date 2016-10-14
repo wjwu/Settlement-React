a7a4aef22d8f1d9d4ece25da06b0d4a03540ddca
wjwu <ferrari575@qq.com>
2016-10-14 18:23:07 +0800

    'login'

---
+++ SettlementApi/SettlementApi.Api/Dtos/CheckCaptchaRequest.cs
@@ -0,0 +1,17 @@
+﻿using SettlementApi.Api.Utility;
+using WebApi.Validation;
+
+namespace SettlementApi.Api.Dtos
+{
+    public class CheckCaptchaRequest
+    {
+        [Required]
+        [MaxLength(13)]
+        [RegularExpression(RegexUtility.Number)]
+        public string TimeSpan { get; set; }
+
+        [Required]
+        [MaxLength(4)]
+        public string Captcha { get; set; }
+    }
+}
